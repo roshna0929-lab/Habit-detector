c3abe147243fa339e9f18ccdcfc583e06a05f746
roshna0929-lab <roshna0929@gmail.com>
2026-05-13 07:53:54 +0530

    Implement login functionality in Login component

---
+++ Login.js
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
+function Login() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const navigate = useNavigate()
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post('http://127.0.0.1:5000/login', {
+        username,
+        password
+      })
+
+      localStorage.setItem('token', res.data.token)
+      navigate('/dashboard')
+    } catch (err) {
+      alert('Invalid Login')
+    }
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <input
+        placeholder='Username'
+        onChange={(e) => setUsername(e.target.value)}
+      />
+
+      <input
+        type='password'
+        placeholder='Password'
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  )
+}
+
+export default Login
